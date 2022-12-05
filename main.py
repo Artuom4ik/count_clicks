8d76286739a1be581f18de5f0cc57054b89ccf07
Artuom4ik <masterskayaigr123@gmail.com>
2022-12-05 22:33:41 +0300

    Добавил функцию shorten_link.

--- main.py
+++ main.py
@@ -4,16 +4,25 @@ import requests
 from dotenv import load_dotenv
 
 
-load_dotenv()
-url = "https://api-ssl.bitly.com/v4/shorten"
-token_bitly = os.getenv("API_BITLINK_TOKEN")
-headers = {
-     "Authorization": token_bitly  
-}
-params = {
-    "long_url": "https://python-scripts.com/json"
-}
+def shorten_link(token_bitly, user_url):
+    headers = {
+        "Authorization": token_bitly  
+    }
+    params = {
+        "long_url": user_url
+    }
+    url = "https://api-ssl.bitly.com/v4/shorten"
+    response = requests.post(url, headers=headers, json=params)
+    response.raise_for_status()
+    return response.json()["link"]
 
-response = requests.post(url, headers=headers, json=params)
-
-print(response.json())
+    
+if __name__ == "__main__":
+    load_dotenv()
+    user_url = input() 
+    token_bitly = os.getenv("API_BITLINK_TOKEN")
+    try:          
+        bitlink = shorten_link(token_bitly, user_url)
+        print('Битлинк', bitlink)
+    except requests.exceptions.HTTPError:
+        print("У вас ошибка в адресе")
