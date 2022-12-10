bac7e66dee3bedcb02f908c73a374593d12cd14f
Artuom4ik <masterskayaigr123@gmail.com>
2022-12-10 13:09:44 +0300

    Добавил функцию по счёту кликов.

--- main.py
+++ main.py
@@ -14,9 +14,19 @@ def shorten_link(token_bitly, user_url):
     url = "https://api-ssl.bitly.com/v4/shorten"
     response = requests.post(url, headers=headers, json=params)
     response.raise_for_status()
-    return response.json()["link"]
+    return response.json()['id']
 
-    
+
+def count_clicks(token_bitly, bitlink):
+    headers = {
+        "Authorization": token_bitly  
+    }
+    url = f"https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary"
+    count_clicks = requests.get(url, headers=headers)
+    count_clicks.raise_for_status()
+    return count_clicks.json()["total_clicks"]
+
+
 if __name__ == "__main__":
     load_dotenv()
     user_url = input() 
@@ -26,3 +36,10 @@ if __name__ == "__main__":
         print('Битлинк', bitlink)
     except requests.exceptions.HTTPError:
         print("У вас ошибка в адресе")
+    try:
+        clicks_count = count_clicks(token_bitly, bitlink)
+        print("Сделано кликов", clicks_count)
+    except requests.exceptions.HTTPError:
+        print("Ошибка")
+
+    
